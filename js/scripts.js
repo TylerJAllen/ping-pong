d6bd14fbaa8c27d8deb796c129725098bd2180c2
Tyler Allen <mynameistylerallen@gmail.com>
2017-05-05 11:01:30 -0700

    add loop in pingPong function to store range from 1 to input into rangeArray and output contents of array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 //Business Logic
 
 
-//Main function that will call upon smaller functions above
+//Main function
 var pingPong = function(input) {
+
+  //Local array that empties after every pass through pingPong function
+  var rangeArray = [];
+
+  //If statement to filter out non-positive integers
   if (input <= 0 || isNaN(input) === true) {
     return "This is not a positive integer, please enter a positive integer and try again.";
   }
-  else {
-    return "Congrats! This is a positive integer!"; //For testing purposes only
+
+  //Loop to populate rangeArray with range from 1 to input
+  for (var index = 1; index <= input; index += 1) {
+    rangeArray.push(index);
   }
+  return rangeArray;
 }
 
 //User Interface Logic
@@ -17,7 +25,9 @@ $(function() {
     event.preventDefault();
     var inputtedNumber = parseInt($("input#number").val());
     var result = pingPong(inputtedNumber);
-    $("#result").text(result);
+    $("#result").hide();
     $("#result").show();
+    $("#result").text(result);
+
   });
 });
