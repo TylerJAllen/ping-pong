993673f904a8d1f86f3c65f709f98e3188b8a66c
Tyler Allen <mynameistylerallen@gmail.com>
2017-05-05 09:48:50 -0700

    create User Interface Logic in js/scripts.js to take input from submit button in in index.html, send the input to pingPong function located in Business Logic and stores results in a variable, then outputs result. Also, create pingPong function in Business Logic to differentiate between positive integer and NaN.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//Business Logic
+
+
+//Main function that will call upon smaller functions above
+var pingPong = function(input) {
+  if (isNaN(input) === true) {
+    return "This is not a positive integer, please enter a positive integer and try again.";
+  }
+  else {
+    return "Congrats! This is a positive integer!"; //For testing purposes only
+  }
+}
+
+//User Interface Logic
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var inputtedNumber = parseInt($("input#number").val());
+    var result = pingPong(inputtedNumber);
+    $("#result").show(result);
+  });
+});
