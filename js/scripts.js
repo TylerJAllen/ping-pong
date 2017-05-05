d41375f90daff59f934c3b121efa775aaf9726b8
Tyler Allen <mynameistylerallen@gmail.com>
2017-05-05 13:17:32 -0700

    add For loop in else section of if statement in pingPong function that populates rangeArray with range of 1 to inputted number. Create arrayToList function that uses forEach loop to display each array element as a list item and add line to call arrayToList function in pingPong function. Debug and make adjustments to pingPong function to satisfy second spec in README.md.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,35 @@
-//Business Logic
+//--------------Business Logic--------------//
 
+//Empty <ul> and populate with new range
+var arrayToList = function(input) {
+  input.forEach(function(element) {
+    $("ul").append("<li>" + element + "</li>");
+  });
+}
 
 //Main function
 var pingPong = function(input) {
-
-  //Local array that empties after every pass through pingPong function
   var rangeArray = [];
-
-  //If statement to filter out non-positive integers
+  $("ul").empty();
+  $(".result").hide();
   if (input <= 0 || isNaN(input) === true) {
-    return "This is not a positive integer, please enter a positive integer and try again.";
+    $("#nonPositiveInteger").show();
   }
-
-  //Loop to populate rangeArray with range from 1 to input
   for (var index = 1; index <= input; index += 1) {
     rangeArray.push(index);
   }
-  return rangeArray;
+  var listArray = arrayToList(rangeArray);
+  $("#rangeList").show();
+  return listArray;
 }
 
-//User Interface Logic
+
+//--------------User Interface Logic--------------//
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var inputtedNumber = parseInt($("input#number").val());
     var result = pingPong(inputtedNumber);
-    $("#result").hide();
-    $("#result").show();
     $("#result").text(result);
-
   });
 });
